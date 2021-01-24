b0ecb5b71196dee57f43742133c8810af5af159e
nekonoko114 <yoshua0826@gmail.com>
2021-01-24 09:59:59 +0900

    add controller & model & view

---
+++ app/ToDo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ToDo extends Model
+{
+    use \Rutorika\Sortable\SortableTrait;
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function goal ()
+    {
+        return $this->belongsTo('App\Goal');
+    }
+}
---
+++ database/migrations/2021_01_24_004431_create_to_dos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToDosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('to_dos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+                $table->text('content');
+                $tabls->integer('user_id');
+                $tabls->integer('goal_id');
+                $tabls->integer('position');
+                $tabls->bollean('done')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('to_dos');
+    }
+}
