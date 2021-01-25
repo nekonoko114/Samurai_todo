16b3c1677bcd685a6fb6af05c80b166d161e79b2
nekonoko114 <yoshua0826@gmail.com>
2021-01-25 17:32:20 +0900

    add Goal.vue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<goals-component></goals-component>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
